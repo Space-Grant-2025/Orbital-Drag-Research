38b4e7490880db96421a15ba8a4e12e01930a2bd
Quinn <qhelfand@gmail.com>
2025-07-06 15:24:36 -0400

    updated code for getting epochs

--- src/get_epochs_from_csv_data.py
+++ src/get_epochs_from_csv_data.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 # given a NORAD ID, reads in processed csv data and return the time when satellite altitude is closest to 280km
@@ -19,5 +20,31 @@ def get_zero_epoch(id):
             if curr_distance_from_280 < min_distance_from_280:
                 closest_alt = current_alt
     return closest_alt
+
+def write_epochs_to_csv(id):
+    prediction_epoch = get_zero_epoch(id)[0]
+    height_280km = get_zero_epoch(id)[1]
+    
+    with open(f'../data/epochs.csv', 'w') as epochs:
+        epochs.write(f'{id},{prediction_epoch},{height_280km}\n')
+
+def main():
+    # delete epochs.csv every time this program runs to get a fresh set of data
+    if os.path.exists(f'../data/epochs.csv'):
+        os.remove(f'../data/epochs.csv')
+
+    # write headers
+    with open(f'../data/epochs.csv', 'w') as epochs:
+        epochs.write("NORAD ID,PREDICTION EPOCH (space-track.org),REFERENCE ALTITUDE EPOCH (100km),ESTIMATED REENTRY EPOCH,HEIGHT CLOSEST TO 280KM,HEIGHT CLOSEST TO 100KM,MIN DST")
+        epochs.close()
+
+    with open(f'../data/reentry_ids_masterlist.txt.txt', 'r') as masterlist:
+        # pass over headers
+        masterlist.readline()
+
+        for id in masterlist:
+            id = id.strip()
+            write_epochs_to_csv(id)
+
 
 print(get_zero_epoch(48384))
