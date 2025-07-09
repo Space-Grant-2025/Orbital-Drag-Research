3603553d2d91bc659085b3dba625222b470eafdd
Quinn <qhelfand@gmail.com>
2025-07-08 20:58:08 -0400

    added nrlmsise00 capability and fixed jb2008 error

--- src/read_tle_by_id.py
+++ src/read_tle_by_id.py
@@ -1,7 +1,6 @@
 import datetime
 import os
 import re
-from pyatmos import download_sw_jb2008, read_sw_jb2008, jb2008
 import ephem
 import math
 
@@ -14,10 +13,6 @@ eccentricity2 = 2 * flattening * (1 - flattening)
 grav = 6.6743e-11
 # mass of Earth
 earth_mass = 5.9722e24
-
-# compile lastest jb2008 data
-swfile = download_sw_jb2008()
-swdata = read_sw_jb2008(swfile)
 
 class tle:
     def __init__(self, id, line1, line2):
@@ -38,7 +33,7 @@ class tle:
         ephem_satellite.compute(ephem_date)
         self.latitude = ephem_satellite.sublat
         self.longitude = ephem_satellite.sublong
-        self.altitude = ephem_satellite.elevation / 1000 # in meters
+        self.altitude = ephem_satellite.elevation / 1000 # in km
         self.mean_motion = 2 * math.pi * float(line2[52:62]) / 86400.
         self.velocity = (grav * earth_mass * self.mean_motion) ** (1 / 3.) * 1e-3
 
@@ -89,10 +84,18 @@ def get_local_time(day_of_year, longitude, utc):
 
     return local_time
 
-def get_density(tle):
-    jb08 = jb2008(tle.get_date(), (tle.get_latitude(), tle.get_longitude(), tle.get_altitude()), swdata)
-    density = jb08.T
+def get_jb2008_density(tle):
+    tle_date = tle.get_date().strftime("%Y-%m-%d %H:%M:%S")
+    jb08 = jb2008(tle_date, (tle.get_latitude(), tle.get_longitude(), tle.get_altitude()), jb2008_swdata)
+    density = jb08.rho
     return str(density)
+
+def get_nrlmsise_density(tle):
+    tle_date = tle.get_date().strftime("%Y-%m-%d %H:%M:%S")
+    nrlmsise = nrlmsise00(tle_date, (tle.get_latitude(), tle.get_longitude(), tle.get_altitude()), nrlmsise_swdata)
+    density = nrlmsise.rho
+    return str(density)
+
 
 # gets in the float representing the date on the first line of the tle and returns datetime object
 def get_date_from_tle(line1):
@@ -151,13 +154,14 @@ def process_tle_data(id):
 def write_data_to_csv(id, tle_list):
     with open('../data/human_readable/tle_' + str(id) + '.csv', 'w') as file:
 
-        file.write("DATE, ALTITUDE, VELOCITY, LATITUDE, LONGITUDE, DENSITY, LOCAL TIME\n")
+        file.write("DATE, ALTITUDE, VELOCITY, LATITUDE, LONGITUDE, JB2008 DENSITY, NRLMSISE00 DENSITY, LOCAL TIME\n")
 
         for tle in tle_list:
             # write values to file
-            file.write(f'{str(tle.get_date())},{tle.get_altitude()},{tle.get_velocity()},{tle.get_latitude()},{tle.get_longitude()},{get_density(tle)},{get_local_time(tle.get_day_of_year(), tle.get_longitude(), tle.get_utc())}\n')
+            file.write(f'{str(tle.get_date())},{tle.get_altitude()},{tle.get_velocity()},{tle.get_latitude()},{tle.get_longitude()},{get_jb2008_density(tle)}, {get_nrlmsise_density(tle)}, {get_local_time(tle.get_day_of_year(), tle.get_longitude(), tle.get_utc())}\n')
 
 def main():
+    start_time = datetime.datetime.now()
     with open('../data/reentry_ids_masterlist.txt', 'r') as file:
 
         if not os.path.exists("../data/human_readable/"):
@@ -167,7 +171,7 @@ def main():
         file.readline()
 
         # progress tracker
-        count = 0
+        count = 1
         # loop through norad ids and create file of tle data
         for id in file:
             id = int(id.strip())
@@ -176,13 +180,27 @@ def main():
                 continue
 
             # create new satellite object and write data to file
-
             write_data_to_csv(id, process_tle_data(id))
-            print(count)
+
+            # progress tracker
+            print(f'{count}: {id}')
             count += 1
 
-
-    print("Finished")
+    # prints total time program takes to run because i'm curious
+    end_time = datetime.datetime.now()
+    print(str(f'Finished: {end_time - start_time}'))
 
 if __name__ == '__main__':
+    from pyatmos import jb2008, nrlmsise00
+    from pyatmos import download_sw_jb2008, read_sw_jb2008
+    from pyatmos import download_sw_nrlmsise00, read_sw_nrlmsise00
+
+    # compile lastest jb2008 data
+    jb2008_swfile = download_sw_jb2008()
+    jb2008_swdata = read_sw_jb2008(jb2008_swfile)
+
+    # compile lastest nrlmsise00 data
+    nrlmsise_swfile = download_sw_nrlmsise00()
+    nrlmsise_swdata = read_sw_nrlmsise00(nrlmsise_swfile)
+
     main()
