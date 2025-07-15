a89d1a556d5c793090937b82b82e7775c8e07fcf
Quinn <qhelfand@gmail.com>
2025-07-15 16:48:17 -0400

    added 14 day limit to plots

--- src/create_plots.py
+++ src/create_plots.py
@@ -1,14 +1,17 @@
 from csv import reader
+from datetime import timedelta, datetime
 
 import matplotlib
 import matplotlib.pyplot as plot
-from matplotlib.ticker import LinearLocator
+from matplotlib.ticker import LinearLocator, ScalarFormatter
+
 
 class satellite_data:
-    def __init__(self, id, date, altitude, longitude, latitude, jb2008, nrlmsise, local_time):
+    def __init__(self, id, date, altitude, velocity, longitude, latitude, jb2008, nrlmsise, local_time):
         self.id = id
         self.date = date
         self.altitude = altitude
+        self.velocity = velocity
         self.longitude = longitude
         self.latitude = latitude
         self.jb2008 = jb2008
@@ -22,6 +25,8 @@ def get_date(self):
     return self.date
 def get_altitude(self):
     return self.altitude
+def get_velocity(self):
+    return self.velocity
 def get_longitude(self):
     return self.longitude
 def get_latitude(self):
@@ -33,35 +38,65 @@ def get_nrlmsise(self):
 def get_local_time(self):
     return self.local_time
 
-
 def gather_data_from_csv(id):
+    # list of data points for one satellite
     satellite_list = []
     with open('../data/human_readable/tle_' + str(id) + '.csv', 'r') as file:
         csv_reader = reader(file)
         # pass over header line
         next(csv_reader)
         for row in csv_reader:
-            date = row[0]
+            date = datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S%z")
             altitude = row[1]
-            longitude = row[2]
-            latitude = row[3]
-            jb2008 = row[4]
-            nrlmsise = row[5]
-            local_time = row[6]
+            velocity = row[2]
+            longitude = row[3]
+            latitude = row[4]
+            jb2008 = row[5]
+            nrlmsise = row[6]
+            local_time = row[7]
 
-            satellite = satellite_data(id, date, altitude, longitude, latitude, jb2008, nrlmsise, local_time)
-            satellite_list.append(satellite)
+            satellite_instance = satellite_data(id, date, altitude, velocity, longitude, latitude, jb2008, nrlmsise, local_time)
+            satellite_list.append(satellite_instance)
     return satellite_list
 
 def plot_density_altitude(satellite_list):
+    # lists to plot
+    jb2008_list = []
+    altitude_list = []
 
-    with plot.rc_context({'axes.autolimit_mode': 'round_numbers'}):
-        jb2008_list, altitude_list = zip(*[(get_jb2008(x), get_altitude(x)) for x in satellite_list])
-        plot.plot(jb2008_list, altitude_list)
-        plot.ylabel("Altitude [km]")
-        plot.xlabel("JB2008 Density")
-        plot.title("NORAD CAT ID " + str(get_id(satellite_list[0])))
-        plot.show()
+    satellite_id = get_id(satellite_list[0])
+    # find reference epoch for satellite
+    with open('../data/epochs.csv', 'r') as file:
+        csv_reader = reader(file)
+        # pass over headers
+        next(csv_reader)
+        for row in csv_reader:
+            epoch_id = int(row[0])
+            if epoch_id == satellite_id:
+                reference_epoch = datetime.strptime(row[1], "%Y-%m-%d %H:%M:%S%z")
+
+    # gather data to plot
+    for satellite_instance in satellite_list:
+        date = get_date(satellite_instance)
+        interval = timedelta(days = 14)
+        if (reference_epoch - interval) <= date <= (reference_epoch + interval):
+                jb2008_list.append(get_jb2008(satellite_instance))
+                altitude_list.append(get_altitude(satellite_instance))
+
+    # plot data
+    plot.scatter(jb2008_list, altitude_list)
+    plot.ylabel("Altitude [km]")
+    plot.xlabel("JB2008 Density")
+    plot.title("NORAD CAT ID " + str(get_id(satellite_list[0])))
+    plot.show()
 
 if __name__ == '__main__':
+    '''with open('../data/reentry_ids_masterlist.txt', 'r') as masterlist:
+        count = 1
+        # pass over headers
+        masterlist.readline()
+
+        for id in masterlist:
+            id = id.strip()
+            plot_density_altitude(gather_data_from_csv(id))'''
     plot_density_altitude(gather_data_from_csv(44235))
