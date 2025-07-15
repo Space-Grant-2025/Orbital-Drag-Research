182864f11cd9eac449bd4e37340193606304b0dd
Quinn <qhelfand@gmail.com>
2025-07-15 19:30:11 -0400

    added f10 plot

--- src/create_plots.py
+++ src/create_plots.py
@@ -1,3 +1,4 @@
+import re
 from csv import reader
 from datetime import timedelta, datetime
 
@@ -38,6 +39,17 @@ def get_nrlmsise(self):
 def get_local_time(self):
     return self.local_time
 
+class f10:
+    def __init__(self, date, value):
+        self.date = date
+        self.value = value
+
+# getters
+    def get_date(self):
+        return self.date
+    def get_value(self):
+        return self.value
+
 def gather_data_from_csv(id):
     # list of data points for one satellite
     satellite_list = []
@@ -59,6 +71,55 @@ def gather_data_from_csv(id):
             satellite_list.append(satellite_instance)
     return satellite_list
 
+def gather_f10_data():
+    f10_list = []
+    start_date = datetime(2000, 1, 1)
+    end_date = datetime(2025, 5, 31)
+
+    with open('../data/SOLFSMY.TXT', 'r') as file:
+        # pass over headers 4x
+        for x in range (0, 4):
+            next(file)
+
+        for row in file:
+            # get values
+            whitespace = re.compile('\\s+')
+            data = whitespace.split(row)
+            year = data[1]
+            day = data[2]
+
+            f10_value = float(data[4])
+            date = datetime.strptime(f"{year}-{day}", "%Y-%j")
+
+            if start_date < date < end_date:
+                # create object and add to list
+                f10_object = f10(date, f10_value)
+                f10_list.append(f10_object)
+    return f10_list
+
+
+def plot_f10_time(f10_list):
+    dates_list = []
+    values_list = []
+
+    for f10 in f10_list:
+        dates_list.append(f10.get_date())
+        values_list.append(f10.get_value())
+
+    fig, ax = plot.subplots(layout='constrained')
+    # x axis (dates)
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%m/%y'))
+    ax.xaxis.set_major_locator(mdates.MonthLocator(interval=6))
+    ax.xaxis.set_minor_locator(mdates.MonthLocator(interval=1))
+
+    plot.plot(dates_list, values_list)
+    plot.title("F10 Values by Time")
+    plot.ylabel("F10")
+    plot.xlabel("Date (MM/YY)")
+    plot.show()
+
+
+
 def plot_reentries_time():
     reentry_list = []
     with open('../data/epochs.csv', 'r') as file:
@@ -75,7 +136,9 @@ def plot_reentries_time():
     # x axis (dates)
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%m/%y'))
     ax.xaxis.set_major_locator(mdates.MonthLocator(interval=6))
+    ax.xaxis.set_minor_locator(mdates.MonthLocator(interval=1))
 
+    # 53 months in 4 years and 5 months
     plot.hist(reentry_list, bins=53)
     plot.title("Starlink Reentries 01/01/2020 to 05/31/2025")
     plot.ylabel("Number of Reentries")
@@ -216,3 +279,4 @@ if __name__ == '__main__':
             plot_density_altitude(gather_data_from_csv(id))'''
     #plot_altitude_time(gather_data_from_csv(44235))
     plot_reentries_time()
+    #plot_f10_time(gather_f10_data())
