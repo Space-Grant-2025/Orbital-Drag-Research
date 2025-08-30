d8f94edbde72dc050f71dbe4115ffed73be81cd5
Quinn <qhelfand@gmail.com>
2025-08-30 13:53:56 -0400

    refactored epoch propagation files

--- src/create_epoch_files.py
+++ src/create_epoch_files.py
@@ -6,9 +6,11 @@ import os
 none_count = 0
 
 class epochs:
-    def __init__ (self, id, tle_instance_epoch, tle_instance_alt, ref_line1, ref_line2):
+    def __init__ (self, id, tle_instance_epoch, tle_instance_alt, ref_line1, ref_line2, instance_line1, instance_line2):
         self.tle_instance_epoch = tle_instance_epoch
         self.tle_instance_alt = tle_instance_alt
+        self.line1 = instance_line1
+        self.line2 = instance_line2
         self.prediction_alt = make_prediction(id, ref_line1, ref_line2, self.tle_instance_epoch)
         if self.prediction_alt is not None:
             self.delta_alt = tle_instance_alt - self.prediction_alt
@@ -22,6 +24,10 @@ def get_tle_instance_epoch(self):
         return self.tle_instance_epoch
 def get_tle_instance_alt(self):
         return self.tle_instance_alt
+def get_instance_line1(self):
+        return self.line1
+def get_instance_line2(self):
+    return self.line2
 def get_prediction_alt(self):
     return self.prediction_alt
 def get_delta_alt(self):
@@ -67,19 +73,21 @@ def read_data_from_csv(id):
         reference_epoch, ref_line1, ref_line2 = get_reference_epoch(id)
         for row in csv_reader:
             current_time = datetime.datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S%z")
-            if current_time > reference_epoch:
+            if current_time >= reference_epoch:
                 current_alt = float(row[2])
-                epoch_object = epochs(id, current_time, current_alt, ref_line1, ref_line2)
+                instance_line1 = row[9]
+                instance_line2 = row[10]
+                epoch_object = epochs(id, current_time, current_alt, ref_line1, ref_line2, instance_line1, instance_line2)
                 epoch_list.append(epoch_object)
     return epoch_list
 
 def write_file(id):
     epoch_list = read_data_from_csv(id)
     with open(f"../data/starlink_reentries_2020_2025/epoch_files/epoch_{id}.csv", "w") as file:
-        file.write("TLE INSTANCE EPOCH,TLE INSTANCE ALTITUDE (KM),PREDICTION FROM REFERENCE EPOCH ALTITUDE (KM),PREDICTION DELTA (KM)\n")
+        file.write("TLE INSTANCE EPOCH,TLE INSTANCE ALTITUDE (KM),TLE INSTANCE LINE 1,TLE INSTANCE LINE 2,PREDICTION FROM REFERENCE EPOCH ALTITUDE (KM),PREDICTION DELTA (KM)\n")
 
         for epoch in epoch_list:
-            file.write(f"{get_tle_instance_epoch(epoch)},{get_tle_instance_alt(epoch)},{get_prediction_alt(epoch)},{get_delta_alt(epoch)}\n")
+            file.write(f"{get_tle_instance_epoch(epoch)},{get_tle_instance_alt(epoch)},{get_instance_line1(epoch)},{get_instance_line2(epoch)},{get_prediction_alt(epoch)},{get_delta_alt(epoch)}\n")
 
 
 def main():
@@ -91,7 +99,7 @@ def main():
         count = 1
         for line in file:
             id = int(line)
-            if os.path.exists(f"../data/starlink_reentries_list.txt/epoch_files/epoch_{id}.csv"):
+            if os.path.exists(f"../data/starlink_reentries_2020_2025/epoch_files/epoch_{id}.csv"):
                 continue
             write_file(id)
             print(f"{count}: {id}")
--- src/create_propagation_files.py
+++ src/create_propagation_files.py
@@ -1,40 +1,79 @@
 from create_epoch_files import *
+from src.read_tle_by_id import get_date_from_tle
+
+none_count = 0
+
+class satellite_prediction():
+    def __init__(self, id, ref_line1, ref_line2):
+
+        self.id = id
+        self.ref_epoch = get_date_from_tle(ref_line1)
+        ephem_date = self.ref_epoch.strftime("%Y-%m-%d %H:%M:%S")
+
+        ref_satellite = ephem.readtle('NORAD' + str(id), ref_line1, ref_line2)
+        ref_satellite.compute(ephem_date)
+
+        self.ref_alt = ref_satellite.elevation / 1000
+        self.pred_epoch, self.pred_alt = predict_100km(self.id, ref_line1, ref_line2, self.ref_epoch)
+
+# getters
+def get_id(self):
+    return self.id
+def get_ref_epoch(self):
+    return self.ref_epoch
+def get_ref_alt(self):
+    return self.ref_alt
+def get_pred_epoch(self):
+    return self.pred_epoch
+def get_pred_alt(self):
+    return self.pred_alt
 
 twelve_hours = datetime.timedelta(hours=12)
 six_months = datetime.timedelta(days=182)
 
 def get_satellite_data(id):
-    epoch_list = []
-    alt_list = []
+    data_list = []
 
-    reference_epoch, ref_line1, ref_line2 = get_reference_epoch(id)
-    reference_epoch = datetime.datetime(reference_epoch.year, reference_epoch.month, reference_epoch.day, reference_epoch.hour, reference_epoch.minute, reference_epoch.second)
-    satellite = ephem.readtle('NORAD' + str(id), ref_line1, ref_line2)
+    with open(f"../data/starlink_reentries_2020_2025/epoch_files/epoch_{id}.csv", "r") as file:
+        csv_reader = csv.reader(file)
+        # pass over headers
+        next(csv_reader)
+        for row in csv_reader:
+            ref_line1 = row[2]
+            ref_line2 = row[3]
+            satellite_data = satellite_prediction(id, ref_line1, ref_line2)
+            data_list.append(satellite_data)
 
-    # starting value that is higher than any starlink sat will be
-    altitude = 9999999
-    current_epoch = reference_epoch
+    return data_list
 
-    while altitude > 100 and current_epoch <= (reference_epoch + six_months):
-        satellite.compute(str(current_epoch))
-        # in km
-        altitude = satellite.elevation / 1000
 
-        epoch_list.append(current_epoch)
-        alt_list.append(altitude)
+def predict_100km(id, ref_line1, ref_line2, ref_epoch):
+    global none_count
+    # starting alt higher than starlink sat
+    altitude = 999999
+    epoch = ref_epoch
+    sat = ephem.readtle('NORAD' + str(id), ref_line1, ref_line2)
 
-        current_epoch += twelve_hours
+    while altitude > 110:
+        try:
+            ephem_epoch = epoch.strftime("%Y-%m-%d %H:%M:%S")
+            sat.compute(ephem_epoch)
+            altitude = sat.elevation / 1000
+            epoch += twelve_hours
+        except:
+            none_count += 1
+            return None, None
 
-    return epoch_list, alt_list
+    return epoch, altitude
 
 def write_data(id):
-    epoch_list, alt_list = get_satellite_data(id)
+    data_list = get_satellite_data(id)
 
     with open(f"../data/starlink_reentries_2020_2025/propagations/propagation_{id}.csv", "w") as file:
-        file.write("EPOCH (BEGINNING WITH REFERENCE TLE),ALTITUDE (KM)\n")
+        file.write("EPOCH (BEGINNING WITH REFERENCE TLE),ALTITUDE (KM),PREDICTION EPOCH,PREDICTION ALT (KM)\n")
 
-        for x in range(len(epoch_list)):
-            file.write(f"{epoch_list[x]},{alt_list[x]}\n")
+        for item in data_list:
+            file.write(f"{get_ref_epoch(item)},{get_ref_alt(item)},{get_pred_epoch(item)},{get_pred_alt(item)}\n")
 
 def main():
     if not os.path.exists("../data/starlink_reentries_2020_2025/propagations"):
@@ -48,4 +87,6 @@ def main():
             id = int(line)
             print(id)
             write_data(id)
+    print(f"None Count: {none_count}")
+
 main()
